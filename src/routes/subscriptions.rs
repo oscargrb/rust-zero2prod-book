9bb9cb611b9700888333c7343bc6b5eb7efe04fc
og.rondon <og.rondon@gmail.com>
2025-10-29 23:16:34 -0400

    adding logs on subscription

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,14 @@ pub struct FormData {
     pub name: String,
 }
 pub async fn subscriptions(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "Request_id {} - Adding '{}'  '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name,
+    );
+
     let uuid = Uuid::new_v4();
 
     match sqlx::query!(
@@ -28,9 +36,19 @@ pub async fn subscriptions(form: Form<FormData>, pool: web::Data<PgPool>) -> Htt
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "Request_id {} - New subscriber datails has been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query {}", e);
+            tracing::error!(
+                "Request_id {} - Failed to execute query {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
